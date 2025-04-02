2ce26ca3e3cfa202c128169833f1409ba092ff4f
Ilan <ilancado99@gmail.com>
2025-04-02 20:30:39 +0200

    Fix messages d'erreur

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Email ou mot de passe invalide.',
+    'password' => 'Mot de passe incorrect.',
+    'throttle' => 'Trop de tentatives. Réessayez dans :seconds secondes.',
+];
